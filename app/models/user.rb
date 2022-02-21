9db19792ab619db7950f3cd759cc0f7d0c42002b
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-22 01:11:52 +0900

    ユーザー新規登録のバリデーション編集2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :nickname, presence:true
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true}
+
   VALID_NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/.freeze
-  validates :first_name, :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'には漢字、ひらがな、全角カタカナでご入力下さい。' }
-  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates :first_name_kana, :last_name_kana, presence: true,
-                                               format: { with: VALID_NAME_KANA_REGEX, message: 'には全角カタカナでご入力下さい。' }
-  validates :birth_date, presence: true
+  validates :first_name,:last_name, 
+    presence:true, 
+    format: {with: VALID_NAME_REGEX, message: "には漢字、ひらがな、全角カタカナでご入力下さい", allow_blank: true}
+  
+    VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  validates :first_name_kana,:last_name_kana,
+    presence:true, 
+    format: {with: VALID_NAME_KANA_REGEX, message:"には全角カタカナでご入力下さい", allow_blank: true}
+
+  validates :birth_date, presence:true
 end

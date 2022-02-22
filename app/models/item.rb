904bc291300b38d531e8705b34891fb58fa9de15
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-22 16:01:55 +0900

    itemモデル、テーブル編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,15 @@ class Item < ApplicationRecord
   validates :name, :explanation, :image,
             presence: true
 
-  validates :category, :condition, :delivery_charge, :prefecture, :delivery_date,
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_date_id,
             numericality: { other_than: 1 , message: "can't be blank" }
+ 
+  VALID_PRICE_REGEX = /\A[0-9]+\z/.freeze
+  validates :price,
+    presence: true,
+    format: { with: VALID_PRICE_REGEX, allow_blank: true },
+    numericality: { only_integer: true, message: "は半角数字でご入力下さい", greater_than_or_equal_to: 300, message: "は300以上でご入力下さい", less_than_or_equal_to: 9_999_999, message: "は9999999以下でご入力下さい", allow_blank: true }
 
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                      presence: { message: "can't be blank" }
-  end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition

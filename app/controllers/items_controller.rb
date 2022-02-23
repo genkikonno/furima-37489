cc0aa3f6ebcc4371ddb9ec4f83d994b2872c9e07
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-23 17:45:13 +0900

    コントローラー修正4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :update, :show]
   def index
     @item = Item.all.order(created_at: :DESC)
   end
@@ -19,18 +19,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     redirect_to item_path and return if @item.update(item_params)
-
     render :edit
   end
 
@@ -39,5 +35,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
                                  :delivery_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end

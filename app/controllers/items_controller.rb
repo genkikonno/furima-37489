6fdb7cfa4189d08152498e2bebb4ca2377aaab53
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-23 18:20:13 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :update, :show, :destroy]
+  before_action :move_to_index, except: [:index, :show]
   def index
     @item = Item.all.order(created_at: :DESC)
   end
@@ -45,5 +46,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end

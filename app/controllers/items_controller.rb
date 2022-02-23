82f87fcb36754cfeb0d8b408d8c3f6d285328c6c
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-23 18:00:56 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
   def index
     @item = Item.all.order(created_at: :DESC)
   end
@@ -28,6 +28,11 @@ class ItemsController < ApplicationController
   def update
     redirect_to item_path and return if @item.update(item_params)
     render :edit
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private

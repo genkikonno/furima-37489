dc0f95030d37323d10a6bf6b346f5a78ad0d3e1a
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-25 03:17:09 +0900

    バリデーション修正2

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -4,8 +4,8 @@ class PurchaseDestination
 
   with_options presence: true do
     validates :user_id, :item_id, :city, :address, :token
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}, allow_blank: true
-    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Not include hyphen(-)"}, allow_blank: true
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)", allow_blank: true}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Not include hyphen(-)", allow_blank: true}
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 

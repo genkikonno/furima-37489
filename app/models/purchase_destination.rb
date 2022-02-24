1df8f06f5ac0567c6e809f30f924281d58cf02f9
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-24 12:15:53 +0900

    バリデーション設定

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -1,4 +1,11 @@
 class PurchaseDestination
   include ActiveModel::Model
-  attr_accessor
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_id        
+
+  with_options presence: true do
+    validates :user_id, :item_id, :purchase_id, :city, :address
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}, allow_blank: true
+    validates :phone_number, format: {with: /\A\d{10,11}\z/. message: "is invalid. Not include hyphen(-)"}, allow_blank: true
+  end
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 end

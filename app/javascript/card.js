017e7a946045da4b59923b619ec9c66cf6189c86
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-25 01:34:13 +0900

    クレジット機能JavaScript修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,7 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  const submit = document.getElementById("button");
-  submit.addEventListener("click", (e) => {
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
@@ -12,7 +12,6 @@ const pay = () => {
       exp_year: `20${formData.get("purchase_destination[exp_year]")}`,
       cvc: formData.get("purchase_destination[cvc]"),
     };
-
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
@@ -20,18 +19,12 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      document.getElementById("purchase_destination_number").removeAttribute("name");
-      document.getElementById("purchase_destination_cvc").removeAttribute("name");
-      document.getElementById("purchase_destination_exp_month").removeAttribute("name");
-      document.getElementById("purchase_destination_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
-    });
-  });
-};
-
-window.addEventListener("load", pay);
-
     });
   });
 };

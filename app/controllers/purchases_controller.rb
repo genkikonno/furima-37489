5d0a6e2d30f32eed1237fee54d4876454d4bc3e9
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-24 17:10:16 +0900

    コントローラー修正3

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,24 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
-  end
-
-  def new
+    @item = Item.find(params[:item_id])
+    @purchase_destination = PurchaseDestination.new
   end
 
   def create
+    @purchase_destination = PurchaseDestination.new(purchase_params)
+    @item = Item.find(params[:item_id])
+    if @purchase_destination.valid?
+      @purchase_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_destination).permit(:post_code, :prefecture_id, :city, :building_name, :address, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end

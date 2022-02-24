850d107a2e4f960699c2813d1b54d7c6fc6aaace
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-24 18:22:30 +0900

    APIキーの環境変数設定

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_5719dbb35898ccbc53a3fc0e");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -3,7 +3,7 @@ class PurchaseDestination
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token       
 
   with_options presence: true do
-    validates :user_id, :item_id, :purchase_id, :city, :address
+    validates :user_id, :item_id, :purchase_id, :city, :address, :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}, allow_blank: true
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Not include hyphen(-)"}, allow_blank: true
   end

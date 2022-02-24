9ea2e140fca73161987cec83da76cc53dcedcb76
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-25 01:35:38 +0900

    バリデーション修正

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -3,7 +3,7 @@ class PurchaseDestination
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token       
 
   with_options presence: true do
-    validates :user_id, :item_id, :purchase_id, :city, :address, :token
+    validates :user_id, :item_id, :city, :address, :token
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}, allow_blank: true
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Not include hyphen(-)"}, allow_blank: true
   end
@@ -11,6 +11,6 @@ class PurchaseDestination
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Destination.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase_id)
+    Destination.create(purchase_id: purchase.id, post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
   end
 end

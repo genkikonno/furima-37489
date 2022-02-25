48d5a649c044d05e471780b31c1b7aab01c6fa37
genkikonno <98734420+genkikonno@users.noreply.github.com>
2022-02-25 12:19:38 +0900

    モデル単体テストコード修正2

--- spec/models/purchase_destination_spec.rb
+++ spec/models/purchase_destination_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe PurchaseDestination, type: :model do
   describe '購入情報の保存' do
     before do
-      user = FactoryBot.create(:user)
-      item = FactoryBot.create(:item)
-      @user = FactoryBot.build(:user)
-      @item = FactoryBot.build(:item, user_id: user)
+      user = FactoryBot.build(:user)
+      item = FactoryBot.build(:item, user_id: user)
       @purchase_destination = FactoryBot.build(:purchase_destination, user_id: user, item_id: item)
       sleep 0.1
     end
